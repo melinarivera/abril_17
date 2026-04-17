84120d2a5a2b286a2e6326c4ddb8cb18989392f1
Melina <melina.rivera@gmail.com>
2026-04-17 10:05:08 +0200

    fix json

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hola mundo");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en http://localhost:3000");
+});
+
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB conectado"))
+  .catch(err => console.error("Error Mongo:", err));
